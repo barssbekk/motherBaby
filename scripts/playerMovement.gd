1587ee87cf7ed3f2be034f072c9cbb017603d4e2
Barsbek Azamatov <bars.moderside@gmail.com>
2025-01-06 16:06:08 -0800

    Init

---
+++ scripts/playerMovement.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+@export var movement_speed : float = 500
+var character_direction : Vector2
+
+func _physics_process(delta: float) -> void:
+	character_direction.x = Input.get_axis("move_left", "move_right")
+	character_direction.y = Input.get_axis("move_up", "move_down")
+	
+	# Flip
+	if (character_direction.x > 0) :
+		#%mamaSprite.flip_h = false
+		pass
+	elif (character_direction.x < 0): 
+		#%mamaSprite.flip_h = true
+		pass
+	
+	if character_direction.length() > 0:
+		velocity = character_direction.normalized() * movement_speed
+		#if %mamaSprite.animation != "Walking": %mamaSprite.animation = "Walking"
+	else: 
+		velocity = Vector2.ZERO
+		#if %mamaSprite.animation != "Idle": %mamaSprite.animation = "Idle"
+		
+	move_and_slide()
