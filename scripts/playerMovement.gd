f1a1864937ff757d3fc93e1f16361ef39b415748
Barsbek Azamatov <bars.moderside@gmail.com>
2025-01-08 19:40:21 -0800

    animationUpdated
    +animations for main character
    +animation for bat

--- scripts/playerMovement.gd
+++ scripts/playerMovement.gd
@@ -1,7 +1,6 @@
 extends CharacterBody2D
 
-
-@export var movement_speed : float = 50
+@export var movement_speed : float = 70
 
 var character_direction : Vector2
 
@@ -10,18 +9,19 @@ func _physics_process(delta: float) -> void:
 	character_direction.y = Input.get_axis("move_up", "move_down")
 	
 	# Flip
-	if (character_direction.x > 0) :
-		#%mamaSprite.flip_h = false
-		pass
+	if (character_direction.x > 0):
+		$AnimatedSprite2D.flip_h = false
 	elif (character_direction.x < 0): 
-		#%mamaSprite.flip_h = true
-		pass
+		$AnimatedSprite2D.flip_h = true
 	
+	# Set animation and movement
 	if character_direction.length() > 0:
 		velocity = character_direction.normalized() * movement_speed
-		#if %mamaSprite.animation != "Walking": %mamaSprite.animation = "Walking"
+		if $AnimatedSprite2D.animation != "run":
+			$AnimatedSprite2D.animation = "run"
 	else: 
 		velocity = Vector2.ZERO
-		#if %mamaSprite.animation != "Idle": %mamaSprite.animation = "Idle"
+		if $AnimatedSprite2D.animation != "idle":
+			$AnimatedSprite2D.animation = "idle"
 		
 	move_and_slide()
